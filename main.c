142e653314b3d72f21115d4dbe96d2c2e3338a41
Sunan Tajwar <sunantajwar@Sunans-Air.home>
2019-11-02 22:56:40 -0400

    began main file, will polish up after finishing node.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    printf("MUSIC LIBRARY TESTS \n ====================");
+    struct node * linked_list = NULL;
+    printf("empty list: \n");
+    print_list(linked_list);
+    int i;
+    printf("[");
+    linked_list = insert_front(linked_list, "Travis Scott", "Anecdote");
+    linked_list = insert_front(linked_list, "Travis Scott", "Goosebumps");
+    linked_list = insert_front(linked_list, "Lil Baby and Gunna", "Drip Harder");
+    linked_list = insert_front(linked_list, "Meek Mill", "Dreams and Nightmares");
+    linked_list = insert_front(linked_list, "Drake", "God's Plan");
+    linked_list = insert_front(linked_list, "Drake", "One Dance");
+    linked_list = insert_front(linked_list, "Eminem", "Rap God");
+    linked_list = insert_front(linked_list, "Eminem", "Lose Yourself");
+    print_list(linked_list);
+    printf("]\n\n");
+    printf("Testing print_node: \n ====================");
+    // insert after writing print_node function
+    printf("Testing find_node: \n ====================");
+    // insert after writing find_node funtion
+}
