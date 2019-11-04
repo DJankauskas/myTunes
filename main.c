0090b478953b13f4e0fac80801d1995803da1e27
Darius Jankauskas <d5926j@gmail.com>
2019-11-03 22:04:49 -0500

    Fixed formatting.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "node.h"
 
 int main(){
     printf("MUSIC LIBRARY TESTS \n ====================");
@@ -8,7 +9,7 @@ int main(){
     printf("empty list: \n");
     print_list(linked_list);
     int i;
-    printf("[");
+    printf("\nlist with some ok songs: \n");
     linked_list = insert_front(linked_list, "Travis Scott", "Anecdote");
     linked_list = insert_front(linked_list, "Travis Scott", "Goosebumps");
     linked_list = insert_front(linked_list, "Lil Baby and Gunna", "Drip Harder");
@@ -18,7 +19,7 @@ int main(){
     linked_list = insert_front(linked_list, "Eminem", "Rap God");
     linked_list = insert_front(linked_list, "Eminem", "Lose Yourself");
     print_list(linked_list);
-    printf("]\n\n");
+    printf("\n\n");
     printf("Testing print_node: \n ====================");
     // insert after writing print_node function
     printf("Testing find_node: \n ====================");
